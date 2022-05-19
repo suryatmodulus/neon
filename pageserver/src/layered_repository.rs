57afb4b4a59b963ff91ae694956ebd19671c639b
Konstantin Knizhnik <knizhnik@garret.ru>
2022-05-19 14:27:28 +0300

    Initialize last_freeze_at with disk consistent LSN to avoid creation of small L0  delta layer on startup
    refer #1736

--- pageserver/src/layered_repository.rs
+++ pageserver/src/layered_repository.rs
@@ -1230,7 +1230,7 @@ impl LayeredTimeline {
             }),
             disk_consistent_lsn: AtomicLsn::new(metadata.disk_consistent_lsn().0),
 
-            last_freeze_at: AtomicLsn::new(0),
+            last_freeze_at: AtomicLsn::new(metadata.disk_consistent_lsn().0),
 
             ancestor_timeline: ancestor,
             ancestor_lsn: metadata.ancestor_lsn(),
@@ -1692,7 +1692,6 @@ impl LayeredTimeline {
     fn flush_frozen_layer(&self, frozen_layer: Arc<InMemoryLayer>) -> Result<()> {
         let new_delta = frozen_layer.write_to_disk()?;
         let new_delta_path = new_delta.path();
-
         // Sync the new layer to disk.
         //
         // We must also fsync the timeline dir to ensure the directory entries for
